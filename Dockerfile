589648b720a8fbfbc9eaeb9f053d9eac9c738fba
Eric Sage <eric.david.sage@gmail.com>
2017-02-23 03:37:13 +0000

    Complete echo server

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
-FROM ubuntu:latest
+FROM ericsage/grpcc
 
-WORKDIR /root
+ADD . /root/app
 
-RUN apt-get update && apt-get install -y wget curl git build-essential autoconf libtool
+WORKDIR /root/app
 
-RUN git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
-RUN cd /root/grpc && git submodule update --init && make && make install
-RUN cd /root/grpc/third_party/protobuf && make && make install && make clean
+RUN cmake .
+RUN make
 
-RUN apt-get update && apt-get install -y wget
-RUN wget http://www.cmake.org/files/v3.6/cmake-3.6.2.tar.gz && tar -xvzf cmake-3.6.2.tar.gz
-RUN cd /root/cmake-3.6.2 && ./configure && make && make install && make clean
-
-WORKDIR /root
-
-
+CMD ["/root/app/nexus"]
--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <grpc/grpc.h>
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_context.h>
+#include <grpc++/security/server_credentials.h>
+#include "cyservice.grpc.pb.h"
+#include "cyservice.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReaderWriter;
+using grpc::Status;
+using cyservice::Fragment;
+using cyservice::CyService;
+
+class CyServiceImpl final : public CyService::Service {
+
+ public:
+
+  Status StreamFragments(ServerContext* context, ServerReaderWriter<Fragment, Fragment>* stream) override {
+    Fragment fragment;
+    while (stream->Read(&fragment)) {
+      stream->Write(fragment);
+    }
+    return Status::OK;
+  }
+
+};
+
 int main (int argc, char *argv[])
 {
-  fprintf(stdout, "Hello world, this is a graph app!");
+  std::string server_address("0.0.0.0:8080");
+  CyServiceImpl service;
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
   return 0;
 }
